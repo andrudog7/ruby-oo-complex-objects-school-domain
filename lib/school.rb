a9433ce0df93067ee7e16e051c8257e9e9489b37
--add <andrewksmoker@gmail.com>
2020-10-27 14:26:27 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+
+    attr_accessor :student, :class, :grade
+    attr_reader :school_name, :roster
+
+    def initialize(school_name)
+        @school_name= school_name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        if @roster.key?(grade) 
+            @roster[grade] << student
+        else
+            @roster[grade] = []
+            @roster[grade] << student
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+       @sorted_roster = {}
+        @roster.each do |grade, student|
+            @sorted_roster[grade] = student.sort
+        end
+        @sorted_roster
+    end
+end
